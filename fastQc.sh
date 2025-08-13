3c16e32b9c7ef660801c5f6a9c8ed56e0a1a6c94
gisales92 <isales.greg@gmail.com>
2025-08-12 21:11:42 -0700

    fixed comments

--- fastQc.sh
+++ fastQc.sh
@@ -7,17 +7,17 @@
 #SBATCH --time=04:15:00     # 4 hours and 15 minutes
 #SBATCH --mail-user=jjenn010@ucr.edu
 #SBATCH --mail-type=ALL
-#SBATCH --job-name="Feature counts"
+#SBATCH --job-name="fast qc"
 #SBATCH -p epyc # You can use any of the following; epyc, intel, batch, highmem, gpu
 
-# Load featureCounts (usually part of subread)
+# Load fastqc
 module load fastqc
 
 # Print timestamp and node
 date
 hostname
 
-# Run featureCounts on all STAR-aligned BAMs
+# Run fastqc on all .fq.gz files in the current dir
 fastqc --threads 8 --outdir prelim_fastqc *.fq.gz
 
 echo "fastQC completed!"
